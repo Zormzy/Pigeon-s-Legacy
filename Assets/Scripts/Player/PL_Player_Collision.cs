9cf3ee65dc47d8acc785a426c85af5ff87b1818d
DoriannX <theodor.xav@gmail.com>
2023-12-08 20:09:36 +0100

    collision of the character
    I made the collision of the character with walls and interactible objects

---
+++ Assets/Scripts/Player/PL_Player_Collision.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem.HID;
+
+public class PL_Player_Collision : MonoBehaviour
+{
+    private Transform transformPlayer;
+    private bool canGoForward, canGoLeft, canGoRight, canGoBack;
+    public static PL_Player_Collision Instance;
+    private void Awake()
+    {
+        transformPlayer = transform;
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            print("you have 2 scripts");
+            Destroy(this);
+        }
+    }
+    private void Update()
+    {
+        CollisionDetectionPlayer();
+    }
+
+    private void CollisionDetectionPlayer()
+    {
+        Debug.DrawRay(transformPlayer.position, transformPlayer.forward, Color.green);
+        if (Physics.Raycast(transformPlayer.position, transformPlayer.forward, out RaycastHit hit, 1))
+        {
+            canGoForward = false;
+            switch (hit.transform.tag)
+            {
+                case "ClosedLockChest": break; //si le joueur a une clef, ouvrir le coffre
+                case "ClosedChest": break; //ouvrir le coffre
+                case "ClosedLockDoor": break; //si le joueur a une clef, ouvrir la porte
+                case "ClosedDoor": break; //ouvrir la porte
+                case "Trap": break; //proposer de désactiver
+                case "DesactivatedTrap": break;
+                case "Exit": break; // finir le niveau
+                default: break;
+            }
+        }else if (Physics.Raycast(transformPlayer.position, -transformPlayer.right, out RaycastHit hitLeft, 1))
+        {
+            canGoLeft = false;
+            canGoForward = true;
+            canGoRight = true;
+            canGoBack = true;
+        }
+        else if (Physics.Raycast(transformPlayer.position, transformPlayer.right, out RaycastHit hitRight, 1))
+        {
+            canGoRight = false;
+            canGoForward = true;
+            canGoLeft = true;
+            canGoBack = true;
+        }
+        else if (Physics.Raycast(transformPlayer.position, -transformPlayer.forward, out RaycastHit hitBack, 1))
+        {
+            canGoBack = false;
+            canGoForward = true;
+            canGoLeft = true;
+            canGoRight = true;
+        }
+        else
+        {
+            canGoBack = true;
+            canGoForward = true;
+            canGoLeft = true;
+            canGoRight = true;
+        }
+    }
+    public bool IsCanGo(Vector3 direction)
+    {
+        if(direction.normalized == Vector3.forward) return canGoForward;
+        else if(direction.normalized == Vector3.right) return canGoRight;
+        else if(direction.normalized == Vector3.back) return canGoBack;
+        else if(direction.normalized == Vector3.left) return canGoLeft;
+        return false;
+    }
+}

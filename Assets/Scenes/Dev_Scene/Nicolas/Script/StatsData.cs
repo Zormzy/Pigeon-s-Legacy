494e77291cf2e6ea3b0fc579e7ac405de53b5c20
NicolasTre <N.TREZEGUET@lahorde.school>
2023-12-08 14:29:19 +0100

    addition of script
    addition of 2 script with scriptableObject
    one is StatsData with the creation of scriptableObject and one is DamageManagement for the managent of damage on the player

---
+++ Assets/Scenes/Dev_Scene/Nicolas/Script/StatsData.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "StatsData", menuName = "Game/Data")]
+public class StatsData : ScriptableObject
+{
+    public string name;
+    public string description;
+    public GameObject gameObjectmodel;
+    public int HP;
+    public int Armor;
+    public int Speed;
+    public int Damage;
+
+}

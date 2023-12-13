647be6123f4bc7e13acd84dfce7c10d26f8cdfdc
DoriannX <theodor.xav@gmail.com>
2023-12-13 15:14:48 +0100

    Trap and cooldown skills
    I added traps and a cooldown for each skills of each character

--- Assets/Scenes/Dev_Scene/Nicolas/Script/ScriptClass/DoctorClass.cs
+++ Assets/Scenes/Dev_Scene/Nicolas/Script/ScriptClass/DoctorClass.cs
@@ -8,30 +8,56 @@ public class DoctorClass : ClassesSkills
     [SerializeField] private HealthKitData healthKitData;
     [SerializeField] private Transform _playerTransform;
 
+    private float[] cooldowns = new float[4];
     private int _damage;
     private RaycastHit _raycastHit;
-
+    private void Awake()
+    {
+        cooldowns[0] = cooldowns[1] = classData.cooldownAttack;
+        cooldowns[2] = cooldowns[3] = classData.cooldownSkill;
+    }
     public override void Skill1()
     {
         _damage = statsData.Damage + classData.classDamage;
         if (Physics.Raycast(_playerTransform.position, _playerTransform.forward, out _raycastHit, 1))
-            if (_raycastHit.transform.CompareTag("Enemy"))
+            if (_raycastHit.transform.CompareTag("Enemy") && cooldowns[0] <= 0)
+            {
                 _raycastHit.transform.GetComponent<PL_Enemy_Attack>().OnTakeDamage(_damage);
+                cooldowns[0] = cooldowns[1];
+                print("doctor attack");
+            }
+    }
+
+    private void Update()
+    {
+        if (cooldowns[0] > 0)
+        {
+            cooldowns[0] -= Time.deltaTime;
+        }
+
+        if (cooldowns[2] > 0)
+        {
+            cooldowns[3] -= Time.deltaTime;
+        }
     }
 
     public override void Skill2()
     {  
         statsData.MaxHP = 15;
-        if (statsData.HP > 0)
+        if (cooldowns[2] <= 0)
         {
-            statsData.HP += healthKitData.regenHP;
-            //Destroy(target.gameObject);
-            // use medkit
-        }
+            if (statsData.HP > 0)
+            {
+                statsData.HP += healthKitData.regenHP;
+                //Destroy(target.gameObject);
+                // use medkit
+            }
 
-        if (statsData.HP + healthKitData.regenHP > statsData.MaxHP)
-        {
-            statsData.HP = 15;
-        } 
+            if (statsData.HP + healthKitData.regenHP > statsData.MaxHP)
+            {
+                statsData.HP = 15;
+            }
+            cooldowns[2] = cooldowns[3];
+        }
     }
 }
--- Assets/Scenes/Dev_Scene/Nicolas/Script/ScriptClass/WarriorClass.cs
+++ Assets/Scenes/Dev_Scene/Nicolas/Script/ScriptClass/WarriorClass.cs
@@ -9,17 +9,43 @@ public class WarriorClass : ClassesSkills
 
     private int _damage;
     private RaycastHit _raycastHit;
+    private float[] cooldowns = new float[4];
 
+    private void Awake()
+    {
+        cooldowns[0] = cooldowns[1] = classData.cooldownAttack;
+        cooldowns[2] = cooldowns[3] = classData.cooldownSkill;
+    }
     public override void Skill1()
     {
         _damage = statsData.Damage + classData.classDamage;
         if (Physics.Raycast(_playerTransform.position, _playerTransform.forward, out _raycastHit, 1))
-            if (_raycastHit.transform.CompareTag("Enemy"))
+            if (_raycastHit.transform.CompareTag("Enemy") && cooldowns[0] <= 0)
+            {
+                cooldowns[0] = cooldowns[1];
                 _raycastHit.transform.GetComponent<PL_Enemy_Attack>().OnTakeDamage(_damage);
+                print("warrior attack");
+            }
     }
 
+    private void Update()
+    {
+        if (cooldowns[0] > 0)
+        {
+            cooldowns[0] -= Time.deltaTime;
+        }
+
+        if (cooldowns[2] > 0)
+        {
+            cooldowns[3] -= Time.deltaTime;
+        }
+    }
     public override void Skill2()
     {
-
+        if(cooldowns[2] <= 0)
+        {
+            //skill 2
+            cooldowns[2] = cooldowns[3];
+        }
     }
 }
